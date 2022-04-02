ef97c9c10b71db949c351be0f236733b869edbaa
Foonkie <foonk@inbox.ru>
2022-04-02 23:58:39 +0300

    15 Step

---
+++ src/test/java/com/foonk/spring/integration/service/CompanyServiceIT.java
@@ -0,0 +1,33 @@
+package com.foonk.spring.integration.service;
+
+import com.foonk.spring.config.DatabaseProperties;
+import com.foonk.spring.dto.CompanyReadDto;
+import com.foonk.spring.integration.annotation.IT;
+import com.foonk.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@IT
+@RequiredArgsConstructor
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
+public class CompanyServiceIT {
+
+    private static final Integer COMPANY_ID = 1;
+
+    private  CompanyService companyService;
+    private  DatabaseProperties databaseProperties;
+
+    @Test
+    void findById() {
+        var actualResult = companyService.findById(COMPANY_ID);
+
+        assertTrue(actualResult.isPresent());
+
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
+    }
+}
