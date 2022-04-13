3ffed66747e6cd963271d962a5c5d2b3228d9682
Foonkie <foonk@inbox.ru>
2022-04-13 23:53:35 +0300

    24 Step

--- src/main/java/com/foonk/spring/http/controller/GreetingController.java
+++ src/main/java/com/foonk/spring/http/controller/GreetingController.java
@@ -1,20 +1,34 @@
 package com.foonk.spring.http.controller;
 
 import com.foonk.spring.database.repository.CompanyRepository;
+import com.foonk.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/api/v1")
 public class GreetingController {
-
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository) {
+    @GetMapping("/hello/")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request)
+  {     modelAndView.addObject("user", new UserReadDto(1L,"Ivan"));
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
     }
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository,
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @CookieValue("JSESSIONID") String jsessionId,
+                              @PathVariable("id") Integer id) {
+        modelAndView.setViewName("greeting/hello");
 
+        return modelAndView;
+    }
+    @GetMapping("/bye")
     public ModelAndView bye() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("greeting/bye");
