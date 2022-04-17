ac0bb9c028e0cecf80e23bf75ecdaac3cbc76c07
Foonkie <foonk@inbox.ru>
2022-04-17 23:56:44 +0300

    28 Step

--- src/main/java/com/foonk/spring/service/CompanyService.java
+++ src/main/java/com/foonk/spring/service/CompanyService.java
@@ -5,20 +5,22 @@ import com.foonk.spring.database.repository.CompanyRepository;
 import com.foonk.spring.database.entity.Company;
 import com.foonk.spring.listner.entity.AccessType;
 import com.foonk.spring.listner.entity.EntityEvent;
+import com.foonk.spring.mapper.CompanyReadMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import com.foonk.spring.dto.CompanyReadDto;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CompanyService {
     private final ApplicationEventPublisher applicationEventPublisher;
-    private final UserService userService;
     private final CompanyRepository companyRepository;
+    private final CompanyReadMapper companyReadMapper;
 
     @Transactional
     public Optional<CompanyReadDto> findById(Integer id){
@@ -27,5 +29,12 @@ public class CompanyService {
                        applicationEventPublisher.publishEvent(new EntityEvent(entity, AccessType.READ));
                 return new CompanyReadDto(entity.getId(), null);
                 });
+
+    }
+    @Transactional
+    public List<CompanyReadDto> findAll() {
+        return companyRepository.findAll().stream()
+                .map(companyReadMapper::map)
+                .toList();
     }
 }
