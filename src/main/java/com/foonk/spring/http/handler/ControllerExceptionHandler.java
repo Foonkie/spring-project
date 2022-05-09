d7b343b0b9061c76a03113fe5a9a7818383835c8
Foonkie <foonk@inbox.ru>
2022-05-09 23:51:51 +0300

    32 Step

--- src/main/java/com/foonk/spring/http/handler/ControllerExceptionHandler.java
+++ src/main/java/com/foonk/spring/http/handler/ControllerExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
-@ControllerAdvice
+@ControllerAdvice(basePackages = "com.foonk.spring.http.controller")
 public class ControllerExceptionHandler /*extends ResponseEntityExceptionHandler*/ {
     @ExceptionHandler(Exception.class)
     public String handleExceptions(Exception exception, HttpServletRequest httpServletRequest){
